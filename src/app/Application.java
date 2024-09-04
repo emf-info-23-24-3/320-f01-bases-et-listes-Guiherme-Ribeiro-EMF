cdb8ace596db0e525e1752952c962ca9eba7c78c
guiherme-Ribeiro-Emf <guilherme.ribeiro@studentfr.ch>
2024-09-04 09:23:04 +0200

    v1

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,25 +1,24 @@
 package app;
 
+import java.util.ArrayList;
+
+import models.Personne;
+
 public class Application {
 
     public static void main(String[] args) {
+        Personne jean = new Personne("Jacquier", "Jean", "Impasse de la Mortadelle", "Fribourg", 1672);
+        Personne paul = new Personne("Bosson", "Paul", "Route de la citadelle", "Bulle", 1676);
+        Personne pierre =new Personne("Ferrero", "Pierre", "Route du coiffeur ", "Berne", 1522);
 
-        //  --------------------------------------------------        
-        //           __     _____ _____ ____  _____ 
-        //           \ \   / / _ \_   _|  _ \| ____|
-        //            \ \ / / | | || | | |_) |  _|  
-        //             \ V /| |_| || | |  _ <| |___ 
-        //              \_/__\___/_|_|_|_| \_\_____|
-        //                / ___/ _ \|  _ \| ____|   
-        //               | |  | | | | | | |  _|     
-        //               | |__| |_| | |_| | |___    
-        //                \____\___/|____/|_____|   
-        //                    |_ _/ ___|_ _|        
-        //                     | | |    | |         
-        //                     | | |___ | |         
-        //                    |___\____|___|        
-        //                                                  
-        //  ---------------------------------------------------
+    ArrayList<Personne> personne = new ArrayList<>();
+    personne.add(jean);
+    personne.add(pierre);
+    personne.add(paul);
+    for (Personne personne2 : personne) {
+        System.out.println(personne2.getPrenom()+ personne2.getNom(),personne2.getRue()+ personne2.getLocalite()+personne2.getNpa());
+        
+    }
 
     }
 
